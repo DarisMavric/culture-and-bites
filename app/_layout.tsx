365b32bc494e4239e8f3b27a1b74270af7506f63
Daris Mavric <mvrcdaris@gmail.com>
2025-03-26 06:43:34 +0100

    Another One

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,6 +19,7 @@ const RootLayout = () => {
         <Stack.Screen name="(auth)" options={{headerShown: false}}/>
         <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
         <Stack.Screen name="(trip)" options={{headerShown: false}}/>
+        <Stack.Screen name="(AI)" options={{headerShown: false}}/>
         <Stack.Screen name="index" options={{headerShown: false}}/>
       </Stack>
     </AuthProvider>
